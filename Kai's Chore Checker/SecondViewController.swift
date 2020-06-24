4cd30592773b51da5f5ad535cc1a00240fb3a25e
Virgil Martinez <virgilmartinez@gmail.com>
2020-06-24 09:38:47 -0500

    Initial Commit

---
+++ Kai's Chore Checker/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kai's Chore Checker
+//
+//  Created by Virgil Martinez on 6/24/20.
+//  Copyright © 2020 Virgil Martinez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
