b66a272974155074b24f1e581748a45b475521e2
Virgil Martinez <virgilmartinez@gmail.com>
2020-06-24 13:04:04 -0500

    Initial commit

--- Kai's Chore Checker/SecondViewController.swift
+++ Kai's Chore Checker/SecondViewController.swift
@@ -14,7 +14,10 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func dishesBtnPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
